94f47254dd23615146cc1d02faa0b4f09ff1ffcc
Lucas Vanhalst <lucas.vanhalst@gmail.com>
2018-07-25 11:22:29 +0200

    Update licenses

--- log_offload/__init__.py
+++ log_offload/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright 2018 Mobicage NV
+# NOTICE: THIS FILE HAS BEEN MODIFIED BY MOBICAGE NV IN ACCORDANCE WITH THE APACHE LICENSE VERSION 2.0
 # Copyright 2018 GIG Technology NV
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +15,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# @@license_version:1.4@@
+# @@license_version:1.5@@
 
 __version__ = '0.1.0'
